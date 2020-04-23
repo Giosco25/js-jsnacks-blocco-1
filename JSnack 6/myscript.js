3415a04d85505b83ac9e9c025c54859bb937549a
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-04-23 16:52:35 +0200

    Snack 6

--- JSnack 6/myscript.js
+++ JSnack 6/myscript.js
@@ -1,26 +1,10 @@
 //Stampa le potenze di 2 fino a 1000
 
 // definisco una variabile
-var numero = 2;
-//for (var i = 2; i <= numero ; i++) {
+var numero = 1000;
+for (var i = 0; Math.pow(2, i) < numero ; i++) {
     //console.log(i);
-    var potenza = Math.pow(numero, 0) // 0
+    var potenza = Math.pow(2, i)
     console.log(potenza);
-    var potenza_prima = Math.pow(2, 1); // 2
-    console.log(potenza_prima);
-    var potenza_seconda = Math.pow(2, 2);  // 4
-    console.log(potenza_seconda);
-    var potenza_terza = Math.pow(2, 3); // 8
-    console.log(potenza_terza);
-    var potenza_quarta = Math.pow(2, 4); // 16
-    console.log(potenza_quarta);
-    var potenza_quinta = Math.pow(2, 5); // 32
-    console.log(potenza_quinta);
-    var potenza_sesta = Math.pow(2, 6); // 64
-    console.log(potenza_sesta);
-    var potenza_settima = Math.pow(2, 7); // 128
-    console.log(potenza_settima);
-    var potenza_ottava = Math.pow(2, 8); // 256
-    console.log(potenza_ottava);
-    var potenza_nona = Math.pow(2, 9); // 512
-    console.log(potenza_nona);
+
+}
